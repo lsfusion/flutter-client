09c9a1f626a6c8e07bb370ba63d1e52dcaae009d
pavel-miniutka <pavel.miniutka@gmail.com>
2026-05-07 10:11:02 +0300

    Changed address bar visibility handling

--- lib/main.dart
+++ lib/main.dart
@@ -45,11 +45,24 @@ class _WebViewPageState extends State<WebViewPage> {
   bool _showAddressBar = false;
   bool _isLoading = false;
   bool _isReady = false;
+  bool _isMouseInAddressBar = false;
+  bool _isAddressBarFocused = false;
 
   String _currentUrl = 'http://127.0.0.1:8080/main';
 
   bool get isWindows => Platform.isWindows;
   bool get isLinux => Platform.isLinux;
+  bool get isMobile => Platform.isAndroid || Platform.isIOS;
+
+  void _setAddressBarVisible(bool visible) {
+    setState(() => _showAddressBar = visible);
+  }
+
+  void _checkHideAddressBar() {
+    if (!_isMouseInAddressBar && !_isAddressBarFocused) {
+      setState(() => _showAddressBar = false);
+    }
+  }
 
   @override
   void initState() {
@@ -89,15 +102,77 @@ class _WebViewPageState extends State<WebViewPage> {
           );
         },
       )
+      ..addJavaScriptChannel(
+        'FlutterAddressBar',
+        onMessageReceived: (flutter.JavaScriptMessage message) {
+          if (message.message == 'show') {
+            _setAddressBarVisible(true);
+          } else if (message.message == 'hide') {
+            _setAddressBarVisible(false);
+          }
+        },
+      )
       ..setNavigationDelegate(flutter.NavigationDelegate(
         onPageStarted: (_) {
           setState(() => _isLoading = true);
         },
         onPageFinished: (_) {
           setState(() => _isLoading = false);
+          _injectSwipeDetector();
         },
       ))
       ..loadRequest(Uri.parse(_currentUrl));
+  }
+
+  void _injectSwipeDetector() {
+    // detects swipe only on non-scrollable elements to show/hide address bar
+    _flutterWebViewController?.runJavaScript('''
+      (function() {
+        if (window.__swipeDetectorInstalled) return;
+        window.__swipeDetectorInstalled = true;
+
+        var startY = 0;
+        var startX = 0;
+        var startEl = null;
+
+        function findScrollableVertical(el) {
+          while (el && el !== document.body && el !== document.documentElement) {
+            var style = window.getComputedStyle(el);
+            var overflowY = style.overflowY;
+            if ((overflowY === 'auto' || overflowY === 'scroll') && el.scrollHeight > el.clientHeight) return el;
+            el = el.parentElement;
+          }
+          var root = document.scrollingElement || document.documentElement;
+          if (root.scrollHeight > root.clientHeight) return root;
+          return null;
+        }
+
+        document.addEventListener('touchstart', function(e) {
+          if (e.touches.length === 1) {
+            startY = e.touches[0].clientY;
+            startX = e.touches[0].clientX;
+            startEl = e.touches[0].target;
+          }
+        }, { passive: true });
+
+        document.addEventListener('touchend', function(e) {
+          if (e.changedTouches.length === 1) {
+            var dy = e.changedTouches[0].clientY - startY;
+            var dx = e.changedTouches[0].clientX - startX;
+            if (Math.abs(dy) > 50 && Math.abs(dy) > Math.abs(dx)) {
+              var scrollable = findScrollableVertical(startEl);
+              if (!scrollable) {
+                if (dy > 0) {
+                  FlutterAddressBar.postMessage('show');
+                } else {
+                  FlutterAddressBar.postMessage('hide');
+                }
+              }
+            }
+          }
+        }, { passive: true });
+      })();
+    ''');
   }
 
   void _initWindowsWebView() async {
@@ -234,62 +309,101 @@ class _WebViewPageState extends State<WebViewPage> {
       );
     }
 
-    return GestureDetector(
-      onVerticalDragEnd: (details) {
-        if (details.primaryVelocity != null) {
-          if (details.primaryVelocity! > 0) {
-            setState(() => _showAddressBar = true);
-          } else if (details.primaryVelocity! < 0) {
-            setState(() => _showAddressBar = false);
-          }
-        }
+    final webViewContent = isWindows
+        ? (_windowsControllerReady && _windowsWebViewController != null
+            ? _buildWindowsWebView()
+            : const Center(child: CircularProgressIndicator()))
+        : (isLinux
+            ? (_cefHelper != null && _cefHelper!.isReady
+                ? _cefHelper!.buildWebView()
+                : const Center(child: CircularProgressIndicator()))
+            : (_flutterWebViewController != null
+                ? flutter.WebViewWidget(controller: _flutterWebViewController!)
+                : const Center(child: CircularProgressIndicator())));
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(child: webViewContent),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                if (!isMobile) _buildDesktopTriggerZone(),
+                _buildAddressBarArea(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDesktopTriggerZone() {
+    if (_showAddressBar) return const SizedBox.shrink();
+    return MouseRegion(
+      onEnter: (_) {
+        _isMouseInAddressBar = true;
+        _setAddressBarVisible(true);
       },
-      child: Scaffold(
-        body: Column(
-          children: [
-            AnimatedContainer(
-              duration: const Duration(milliseconds: 300),
-              curve: Curves.easeInOut,
-              height: _showAddressBar ? 60 : 0,
-              child: AnimatedOpacity(
-                duration: const Duration(milliseconds: 200),
-                opacity: _showAddressBar ? 1 : 0,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: AddressBar(
-                    initialUrl: _currentUrl,
-                    isLoading: _isLoading,
-                    onNavigate: (url) {
-                      setState(() {
-                        _currentUrl = url;
-                      });
-                      if (isWindows) {
-                        _windowsWebViewController?.loadUrl(url);
-                      } else if (isLinux) {
-                        _cefHelper?.loadUrl(url);
-                      } else {
-                        _flutterWebViewController?.loadRequest(Uri.parse(url));
-                      }
-                    },
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              child: isWindows
-                  ? (_windowsControllerReady && _windowsWebViewController != null
-                      ? _buildWindowsWebView()
-                      : const Center(child: CircularProgressIndicator()))
-                  : (isLinux
-                      ? (_cefHelper != null && _cefHelper!.isReady
-                          ? _cefHelper!.buildWebView()
-                          : const Center(child: CircularProgressIndicator()))
-                      : (_flutterWebViewController != null
-                          ? flutter.WebViewWidget(controller: _flutterWebViewController!)
-                          : const Center(child: CircularProgressIndicator()))),
-            ),
-          ],
-        ),
+      child: Container(
+        height: 4,
+        color: Colors.white.withAlpha((255.0 * 0.3).round()),
+      ),
+    );
+  }
+
+  Widget _buildAddressBarArea() {
+    final addressBar = Container(
+      color: Theme.of(context).scaffoldBackgroundColor.withAlpha((255.0 * 0.9).round()),
+      padding: const EdgeInsets.all(8.0),
+      child: AddressBar(
+        initialUrl: _currentUrl,
+        isLoading: _isLoading,
+        onNavigate: (url) {
+          setState(() {
+            _currentUrl = url;
+          });
+          if (isWindows) {
+            _windowsWebViewController?.loadUrl(url);
+          } else if (isLinux) {
+            _cefHelper?.loadUrl(url);
+          } else {
+            _flutterWebViewController?.loadRequest(Uri.parse(url));
+          }
+        },
+      ),
+    );
+
+    final animatedBar = AnimatedCrossFade(
+      duration: const Duration(milliseconds: 300),
+      firstChild: addressBar,
+      secondChild: const SizedBox(width: double.infinity, height: 0),
+      crossFadeState: _showAddressBar ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+      sizeCurve: Curves.easeInOut,
+    );
+
+    if (isMobile) return animatedBar;
+
+    return MouseRegion(
+      onEnter: (_) {
+        _isMouseInAddressBar = true;
+      },
+      onExit: (_) {
+        _isMouseInAddressBar = false;
+        _checkHideAddressBar();
+      },
+      child: Focus(
+        onFocusChange: (hasFocus) {
+          _isAddressBarFocused = hasFocus;
+          if (!hasFocus) {
+            _checkHideAddressBar();
+          }
+        },
+        child: animatedBar,
       ),
     );
   }
